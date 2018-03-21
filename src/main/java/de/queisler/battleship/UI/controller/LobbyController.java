71487eac6cb489c1cac77275d6721c8d4d260521
queisler_y <queisler_y@WDO-3989.DOMS.KVWL.DE>
2018-03-21 14:54:40 +0100

    Adding auto refreshing player table in lobby.html

--- src/main/java/de/queisler/battleship/UI/controller/LobbyController.java
+++ src/main/java/de/queisler/battleship/UI/controller/LobbyController.java
@@ -1,40 +1,61 @@
 package de.queisler.battleship.UI.controller;
 
-import de.queisler.battleship.businessLogic.model.Player;
-import de.queisler.battleship.data.services.PlayerService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
 
-import java.util.ArrayList;
-import java.util.List;
+import de.queisler.battleship.businessLogic.model.Player;
+import de.queisler.battleship.data.services.PlayerService;
 
-@Controller
-public class LobbyController {
-    //TODO: Async update task of table
-    @Autowired
-    PlayerService playerService;
-    List<Player> players;
+@Controller("lobbyController")
+public class LobbyController
+{
+	//TODO: Async update task of table
+	@Autowired
+	private PlayerService playerService;
+	private Map<String, Player> players = new HashMap<>();
 
-    public LobbyController() {
-        players = players();
-    }
+	@GetMapping("/lobby")
+	public String lobby(Model model)
+	{
+		Map<String, Player> tableData = new HashMap<>(players);
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		tableData.remove(authentication.getName());
+		model.addAttribute("players", new ArrayList<Player>(tableData.values()));
+		return "lobby";
+	}
 
-    @GetMapping("/lobby")
-    public String lobby(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Player p = playerService.getPlayer(authentication.getName());
-        players.add(p);
-        model.addAttribute("players", players);
-        return "lobby";
-    }
+	@GetMapping(value = "/lobby", params = "update")
+	public String tableUpdate(Model model)
+	{
+		Map<String, Player> tableData = new HashMap<>(players);
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		tableData.remove(authentication.getName());
+		model.addAttribute("players", new ArrayList<Player>(tableData.values()));
+		return "lobby :: playerTable";
+	}
 
-    @Bean
-    public List<Player> players() {
-        return new ArrayList<>();
-    }
+	@EventListener
+	public void handleEvent(ServletRequestHandledEvent e)
+	{
+		String username = e.getUserName();
+		String url = e.getRequestUrl();
+
+		if (username != null)
+		{
+			if (url.equals("/lobby"))
+				players.put(username, playerService.getPlayer(username));
+			else
+				players.remove(username);
+		}
+	}
 }
