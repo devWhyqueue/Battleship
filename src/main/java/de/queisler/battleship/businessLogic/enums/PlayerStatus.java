3f27d6c3513e90ff9abc55c2564e58a41e72e3bf
queisler_y <queisler_y@WDO-3989.DOMS.KVWL.DE>
2018-03-23 16:33:00 +0100

    Deleting PlayerStatus attribute and enumeration

--- src/main/java/de/queisler/battleship/businessLogic/enums/PlayerStatus.java
+++
@@ -1,5 +0,0 @@
-package de.queisler.battleship.businessLogic.enums;
-
-public enum PlayerStatus {
-	OFFLINE, ONLINE, READY, INGAME
-}
--- src/main/java/de/queisler/battleship/businessLogic/model/Player.java
+++ src/main/java/de/queisler/battleship/businessLogic/model/Player.java
@@ -1,13 +1,17 @@
 package de.queisler.battleship.businessLogic.model;
 
-import de.queisler.battleship.businessLogic.enums.PlayerStatus;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -21,7 +25,7 @@ public class Player
 
 	@Column(unique = true)
 	@NotNull
-    @Size(min = 3)
+	@Size(min = 3)
 	private String username;
 	@NotNull
 	@Size(min = 5)
@@ -30,8 +34,6 @@ public class Player
 	private String firstName;
 	@NotNull
 	private String lastName;
-	@Transient
-	private PlayerStatus status;
 	@Transient
 	private Fleet fleet;
 }
