4ad6904f421c66a4638430420d0e7cd0b0e7b8f6
queisler_y <queisler_y@WDO-4906.mshome.net>
2018-04-06 14:12:09 +0200

    Implementing GameController and game.html

--- src/main/java/de/queisler/battleship/UI/controller/GameController.java
+++ src/main/java/de/queisler/battleship/UI/controller/GameController.java
@@ -9,11 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import de.queisler.battleship.businessLogic.enums.AttackResult;
 import de.queisler.battleship.businessLogic.enums.PointStatus;
 import de.queisler.battleship.businessLogic.exceptions.GameException;
 import de.queisler.battleship.businessLogic.exceptions.InvalidPointException;
@@ -33,14 +33,7 @@ public class GameController
 	{
 		Player p = getPlayerFromAuthentication();
 		Game g = gameManagement.getGame(p);
-		try
-		{
-			g.startGame();
-		}
-		catch (GameException e)
-		{
-			// Game already started by opponent
-		}
+		g.startGame();
 
 		PointStatus[][] shipMap = p.getFleet().getShipMap().getPointStatusArray();
 		List<PointStatus> list = new ArrayList<PointStatus>();
@@ -52,8 +45,6 @@ public class GameController
 		List<PointStatus> list2 = new ArrayList<PointStatus>();
 		for (int i = 0; i < hitMap.length; i++)
 			list2.addAll(Arrays.asList(hitMap[i]));
-
-		System.err.println(Arrays.toString(list2.toArray()));
 		model.addAttribute("opPoints", list2);
 
 		return "game";
@@ -73,6 +64,21 @@ public class GameController
 		return "game :: enemyFieldMap";
 	}
 
+	@GetMapping(value = "/game", params = "updateTurnStatus")
+	public ResponseEntity<?> updateTurnStatus(Model model) throws GameException
+	{
+		Player p = getPlayerFromAuthentication();
+		Game g = gameManagement.getGame(p);
+
+		if (g.determineWinner() != null)
+			return ResponseEntity.status(210).build(); // Game lost
+
+		if (g.getCurrentPlayer().equals(p))
+			return ResponseEntity.status(211).build(); // Player's turn
+		else
+			return ResponseEntity.status(212).build(); // Opponent's turn
+	}
+
 	@PostMapping(value = "/game")
 	public ResponseEntity<?> attack(Model model, @RequestParam("attack") int pt)
 		throws GameException, InvalidPointException
@@ -81,14 +87,17 @@ public class GameController
 		Game g = gameManagement.getGame(p);
 		int row = pt / 10 + 1;
 		int column = pt % 10 + 1;
-		g.attack(p, new Point(row, column));
+		AttackResult r = g.attack(p, new Point(row, column));
 
-		return ResponseEntity.ok().build();
-	}
-
-	@ExceptionHandler({ GameException.class })
-	public void handleException(Exception e)
-	{
+		if (r.name().startsWith("SUNK"))
+			return ResponseEntity.status(213).body(
+				"You destroyed your opponent's " + g.getOpponent(p).getFleet().getShip(r.getShipType()).toString()); // DESTROYED
+		else if (r == AttackResult.LOST)
+			return ResponseEntity.status(214).body("Congratulations! You won the game!");
+		else if (r == AttackResult.HIT)
+			return ResponseEntity.status(215).build();
+		else
+			return ResponseEntity.status(216).build(); // MISS
 
 	}
 
